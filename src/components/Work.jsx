f56cd28d0566e6898c166ced7168479b3d94deca
noel-toms <noel.toms@mail.utoronto.ca>
2023-07-30 20:18:45 -0400

    updated skills and work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import WorkImg from '../assets/projects/workImg.jpeg'
-import realEstate from '../assets/projects/realestate.jpg'
+import smusic from '../assets/projects/Smusic.png'
+import stonks from '../assets/projects/Stonks.png'
+import tubestats from '../assets/projects/TubeStats.png'
+
 
 const Work = () => {
   return (
@@ -13,26 +15,63 @@ const Work = () => {
 
             {/*  Container */}
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                <h1>WIP</h1>
 
                 {/* Grid Item */}
-                {/* <div  style={{backgroundImage: `url(${WorkImg})`}} className='shadow=lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div  style={{backgroundImage: `url(${smusic})`}} className='shadow=lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
-                    // Hover Effects
+                    {/* // Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                        React JS Application
+                        Python Application
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href="#">
+                            {/* <a href="#">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
+                            </a> */}
                             <a href="#">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
                     </div>
-                </div> */}
+                </div>
+
+                {/* Grid Item */}
+                <div  style={{backgroundImage: `url(${stonks})`}} className='shadow=lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    
+                    {/* // Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                        Java Swing Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            {/* <a href="#">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a> */}
+                            <a href="https://github.com/NoeljToms/course-project-Team-Stonks">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Grid Item */}
+                <div  style={{backgroundImage: `url(${tubestats})`}} className='shadow=lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    
+                    {/* // Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                        Django Fullstack Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            {/* <a href="#">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a> */}
+                            <a href="https://github.com/NoeljToms/TubeStats">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         </div>
